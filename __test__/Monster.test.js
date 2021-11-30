9e4c52710ce5ef4c04ba831f7423883df1c6e421
Rym25 <ryely.moore@outlook.com>
2021-11-29 17:01:02 -0800

    wrote tests for Monster and Player objects, then created objects to pass basic test

--- __test__/Monster.test.js
+++ __test__/Monster.test.js
@@ -1,7 +1,7 @@
 const Monster = require('../lib/Monster.js');
 // Test to determine if a monster with a simple stat block is created
 test('creates a monster object', () => {
-    const monster = new Monster({name: 'Goblin', hp: 7, ac: 15, spd: 30, str: 8, dex: 14, con: 10, int: 10, wis: 8, cha: 8, wep: 'Scimitar', prof: 2});
+    const monster = new Monster('Goblin', 7, 15, 30, 8, 14, 10, 10, 8, 8, 'Scimitar', 2);
 
     expect(monster.name).toBe('Goblin');
     expect(monster.hp).toBe(7);
@@ -14,5 +14,5 @@ test('creates a monster object', () => {
     expect(monster.wis).toBe(8);
     expect(monster.cha).toBe(8);
     expect(monster.wep).toBe('Scimitar');
-    expect(monster.cha).toBe(2);
+    expect(monster.prof).toBe(2);
 });
---
+++ __test__/Player.test.js
@@ -0,0 +1,18 @@
+const Player = require('../lib/Player.js');
+
+test('creates Player object', () => {
+    const player = new Player('Randy', 12, 16, 30, 16, 13, 14, 9, 15, 11, 'Greataxe', 2);
+
+    expect(player.name).toBe('Randy');
+    expect(player.hp).toBe(12);
+    expect(player.ac).toBe(16);
+    expect(player.spd).toBe(30);
+    expect(player.str).toBe(16);
+    expect(player.dex).toBe(13);
+    expect(player.con).toBe(14);
+    expect(player.int).toBe(9);
+    expect(player.wis).toBe(15);
+    expect(player.cha).toBe(11);
+    expect(player.wep).toBe('Greataxe');
+    expect(player.prof).toBe(2);
+})
